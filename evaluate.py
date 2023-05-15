4378ec8222a405bcb854d8e2812dfe349ffd5e6e
Efe Sener <sefe1749@gmail.com>
2023-05-15 15:54:46 +0200

    backend new features and definitions

--- evaluate.py
+++ evaluate.py
@@ -18,29 +18,31 @@ def evaluate(code, maze):
     load_dotenv()
     openai.api_key = os.environ["OPENAI_API_KEY"]
     # openai.organization_id = os.environ["ORGANIZATION_ID"]
+    with open("pseudo_lang.txt", "r", encoding="utf-8") as file:
+        pseudo_structure = file.read()
+    with open("pseudo_lang_2.txt", "r", encoding="utf-8") as file:
+        pseudo_structure_2 = file.read()
 
-    # # OpenAI API call
-    # response = openai.ChatCompletion.create(
-    #     # model="gpt-3.5-turbo",
-    #     model="gpt-4",
-    #     messages=[
-    #         {"role": "system", "content": "You are a helpful assistant providing feedback and a score for the given python code."},
-    #         {"role": "user", "content": f"I am going to give you a python code which was previously translated from the pseudo code, so it is also normal that there is no comment in the code. Assume that the defined functions without the code inside are defined correctly and working correctly. Focus on the usage & results of the functions. Main goal of this code is the player in the car tries to travel from start point {start} to end point {end} in this specific maze: {maze}. This code is not a general algorithm, but a specific one to solve this maze. One direction forward means going from (5, 5) to (4, 5)."},
-    #         {"role": "assistant", "content": "I understand. The player tries to get from the start point to the end point in the maze. I will assume that the defined functions are implemented corretly and focus on the usage of the functions and results of these functions."},
-    #         {"role": "user", "content": f"Please provide a short feedback and a score (0-100) of the given code. Also try to understand if the player can get from start point to end point using that python code and provide a percentage of success. Example solution would seems like this {path_taken}. At the end of the feedback, write the path that the player took executing the python code. Give the answer so that I will extract them as 'Score:\s*(\d+)', 'Feedback:\s*(.*)' and 'Percentage:\s*(\d+)'."},
-    #         {"role": "assistant", "content": "I will give a short feedback, a score out of 100 and a success percentage of the player for the given code. Please provide the Python code."},
-    #         {"role": "user", "content": f"Here is the Python code: {code}"},
-    #     ]
-    # )
+    # OpenAI API call
+    response = openai.ChatCompletion.create(
+        # model="gpt-3.5-turbo",
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant providing feedback and a score for the given python code."},
+            {"role": "user", "content": f"I am going to give you a python code which was previously translated from the pseudo code, so it is also normal that there is no comment in the code. Assume that the defined functions without the code inside are defined correctly and working correctly. Focus on the usage & results of the functions. Main goal of this code is the player in the car tries to travel from start point {start} to end point {end} in this specific maze: {maze}. This code is not a general algorithm, but a specific one to solve this maze. One direction forward means going from (5, 5) to (4, 5)."},
+            {"role": "assistant", "content": "I understand. The player tries to get from the start point to the end point in the maze. I will assume that the defined functions are implemented corretly and focus on the usage of the functions and results of these functions."},
+            {"role": "user", "content": f"Please provide a short feedback and a score (0-100) of the given code. Also try to understand if the player can get from start point to end point using that python code and provide a percentage of success. Example solution would seems like this {path_taken}. At the end of the feedback, write the path that the player took executing the python code. Give the answer so that I will extract them as 'Score:\s*(\d+)', 'Feedback:\s*(.*)' and 'Percentage:\s*(\d+)'."},
+            {"role": "assistant", "content": "I will give a short feedback, a score out of 100 and a success percentage of the player for the given code. Please provide the Python code."},
+            {"role": "user", "content": f"Here is the Python code: {code}"},
+        ]
+    )
 
     # MVP response
-    with open("pseudo_lang_2.txt", "r", encoding="utf-8") as file:
-        pseudo_structure = file.read()
     # response = openai.ChatCompletion.create(
     # model="gpt-4",
     # messages=[
     #         {"role": "system", "content": "You are a helpful assistant understanding pseudo language."},
-    #         {"role": "user", "content": f"In the future I want you to understand the given pseudo code. This is the structure of the pseudo code that will be given: {pseudo_structure}"},
+    #         {"role": "user", "content": f"In the future I want you to understand the given pseudo code. This is the structure of the pseudo code that will be given: {pseudo_structure_2}"},
     #         {"role": "assistant", "content": "Thanks for the pseudo code structure."},
     #         {"role": "user", "content": f"Main goal is that a game is being played. The player's car is in a maze, the start point is {start}, end point is {end}, the maze is {maze}.'0' being walls and '1' being walkable paths. Main goal of the game: player writes a pseudo code so that the car drives from the start point to end point for this specific maze. This code is not a general algorithm, but a specific one to solve this maze."},
     #         {"role": "assistant", "content": "I understand. The player tries to get from the start point to the end point in the maze. The player writes a pseudo code to do so."},
@@ -51,17 +53,19 @@ def evaluate(code, maze):
     # )
 
     # TODO: Maze as 2d input
-    response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-            {"role": "system", "content": "You are a helpful assistant understanding pseudo language."},
-            {"role": "user", "content": f"In the future I want you to understand the given pseudo code. This is the structure of the pseudo code that will be given: {pseudo_structure}"},
-            {"role": "assistant", "content": "Thanks for the pseudo code structure."},
-            {"role": "user", "content": f"The player's car is in a maze, the start point is {start}, end point is {end}, the maze is {maze}.'0' being walls and '1' being walkable paths. Main goal of the game: player writes a pseudo code so that the car drives from the start point to end point for this specific maze. This code is not a general algorithm, but a specific one to solve this maze. This is the {code}. Please provide a short feedback and a score (0-100) of the given pseudo code, considering the start point '{start}', end point '{end}' and the maze is given. One direction forward means going from (5, 5) to (4, 5). Also try to understand if the player can get from start point to end point using that code and provide a percentage of success. Example solution would seems like this {path_taken}. At the end of the feedback, write the path that the player took given the pseudo code, even tough it might be wrong. Give the answer so that I will extract them as 'Score:\s*(\d+)', 'Feedback:\s*(.*)' and 'Percentage:\s*(\d+)'."}, 
-            #{"role": "user", "content": f"Translate this code to python, keep the code simple and understandable, also write start and end points and the maze itself in the python code so that it would work by itself. At first define the functions and then comment '#Translated Pseudo Code' and translate the code in python. At the end print the result. Example solution would seems like this {path_taken}. Just give the converted python code as the result so that I can extract it like this '```python(.*?)```': {code}"},
+    ## Mock response
+    # response = openai.ChatCompletion.create(
+    # model="gpt-4",
+    # messages=[
+    #         {"role": "system", "content": "You are a helpful assistant understanding pseudo language."},
+    #         {"role": "user", "content": f"In the future I want you to understand the given pseudo code. This is the structure of the pseudo code that will be given: {pseudo_structure}"},
+    #         {"role": "assistant", "content": "Thanks for the pseudo code structure."},
+    #         {"role": "user", "content": f"The player's car is in a maze, the start point is {start}, end point is {end}, the maze is {maze}.'0' being walls and '1' being walkable paths. Main goal of the game: player writes a pseudo code so that the car drives from the start point to end point for this specific maze. This code is not a general algorithm, but a specific one to solve this maze. This is the {code}. Please provide a short feedback and a score (0-100) of the given pseudo code, considering the start point '{start}', end point '{end}' and the maze is given. One direction forward means going from (5, 5) to (4, 5). Also try to understand if the player can get from start point to end point using that code and provide a percentage of success. Example solution would seems like this {path_taken}. At the end of the feedback, write the path that the player took given the pseudo code, even tough it might be wrong. Give the answer so that I will extract them as 'Score:\s*(\d+)', 'Feedback:\s*(.*)' and 'Percentage:\s*(\d+)'."}, 
+    #         #{"role": "user", "content": f"Translate this code to python, keep the code simple and understandable, also write start and end points and the maze itself in the python code so that it would work by itself. At first define the functions and then comment '#Translated Pseudo Code' and translate the code in python. At the end print the result. Example solution would seems like this {path_taken}. Just give the converted python code as the result so that I can extract it like this '```python(.*?)```': {code}"},
 
-        ]
-    )
+    #     ]
+    # )
+
     # Extract the answer from the API response
     answer = response['choices'][0]['message']['content']
 
--- translate.py
+++ translate.py
@@ -19,26 +19,30 @@ def translate(code, maze):
     #TODO Translate the maze to python which can then be used in exec() in evaluate.py
 
     # Read the content of the "pseudo_lang.txt" file
-    with open("pseudo_lang_2.txt", "r", encoding="utf-8") as file:
+    with open("pseudo_lang.txt", "r", encoding="utf-8") as file:
         pseudo_structure = file.read()
+    with open("pseudo_lang_2.txt", "r", encoding="utf-8") as file:
+        pseudo_structure_2 = file.read()
+    with open("functions.txt", "r", encoding="utf-8") as file:
+        functions_predefined = file.read()
 
     # test_code = "while ($i < 8) { print($i); $i++; } name2 = "Max";"
     # OpenAI API call
 
-    # # General response
-    # response = openai.ChatCompletion.create(
-    # model="gpt-4",
-    # messages=[
-    #         {"role": "system", "content": "You are a helpful assistant converting pseudo language to python language."},
-    #         {"role": "user", "content": f"In the future I want you to convert the given pseudo code in python language. This is the structure of the pseudo code that will be given: {pseudo_structure}"},
-    #         {"role": "assistant", "content": "Thanks for the pseudo code structure. I will use it to convert the given pseudo code to python code."},
-    #         {"role": "user", "content": f"Main goal is that a game is being played. The user is in a maze, the start point is {start}, end point is {end}, the maze is {maze}.'0' being walls and '1' being walkable paths. Main goal of the game: player writes a pseudo code so that the player walks from the start point to end point for this specific maze."},
-    #         {"role": "assistant", "content": "I understand. The player tries to get from the start point to the end point in the maze. The player writes a pseudo code to do so. I will convert the pseudo code to python code considering this."},
-    #         {"role": "user", "content": f"Translate this code to python, keep the code simple and understandable, also write start and end points and the maze itself in the python code. Just define the functions without writing the code and assume that they will work fine and then comment '#Translated Pseudo Code' and translate the code in python. At the end print path the player has to take as the result. Example solution would seems like this {path_taken}. Just give the converted python code so that I can extract it like this '```python(.*?)```': {code}"},
-    #         #{"role": "user", "content": f"Translate this code to python, keep the code simple and understandable, also write start and end points and the maze itself in the python code so that it would work by itself. At first define the functions and then comment '#Translated Pseudo Code' and translate the code in python. At the end print the result. Example solution would seems like this {path_taken}. Just give the converted python code as the result so that I can extract it like this '```python(.*?)```': {code}"},
+    # General response
+    response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant converting pseudo language to python language."},
+            {"role": "user", "content": f"In the future I want you to convert the given pseudo code in python language. This is the structure of the pseudo code that will be given: {pseudo_structure_2}. And also consider this structure to see how to translate the code to python: {pseudo_structure} "},
+            {"role": "assistant", "content": "Thanks for the pseudo code structure. I will use it to convert the given pseudo code to python code."},
+            {"role": "user", "content": f"Main goal is that a game is being played. The user is in a maze, the start point is {start}, end point is {end}, the maze is {maze}.'0' being walls and '1' being walkable paths. Main goal of the game: player writes a pseudo code so that the player walks from the start point to end point for this specific maze."},
+            {"role": "assistant", "content": "I understand. The player tries to get from the start point to the end point in the maze. The player writes a pseudo code to do so. I will convert the pseudo code to python code considering this."},
+            {"role": "user", "content": f"Translate this code to python, use these predefined functions: {functions_predefined}. Keep the code simple and understandable, also write start and end points and the maze itself in the python code. Just define the functions without writing the code and assume that they will work fine and then comment '#Translated Pseudo Code' and translate the code in python. At the end print path the player has to take as the result. Example solution would seems like this {path_taken}. Just give the converted python code so that I can extract it like this '```python(.*?)```': {code}"},
+            #{"role": "user", "content": f"Translate this code to python, keep the code simple and understandable, also write start and end points and the maze itself in the python code so that it would work by itself. At first define the functions and then comment '#Translated Pseudo Code' and translate the code in python. At the end print the result. Example solution would seems like this {path_taken}. Just give the converted python code as the result so that I can extract it like this '```python(.*?)```': {code}"},
 
-    #     ]
-    # )
+        ]
+    )
 
 
     
